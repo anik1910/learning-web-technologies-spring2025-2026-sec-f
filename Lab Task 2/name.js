0be121cb9d60b82b3cc9fa0f258252809a77d6f9
Anik <aniktitu@gmail.com>
2025-03-24 14:00:45 +0600

    name.js updated without regexp

--- Lab Task 2/name.js
+++ Lab Task 2/name.js
@@ -1,21 +1,42 @@
-const nameFormat = /^[a-zA-Z][a-zA-Z.\-]*\s+[a-zA-Z.\-]+$/;
 function namevalidation()
 {
     let name = document.getElementById('name').value;
     let error = document.getElementById('error');
-   if (name == "")
+
+    if (name.trim() == "")
+        {
+            error.innerHTML = "The field is empty";
+            error.style.color = 'red';
+            return;
+        }
+    let word = name.split(" ");
+    let wordCount = 0;
+
+    for(let i = 0; i < word.length; i++)
     {
-        error.innerHTML = "The field is empty";
+        if(word[i] !== "")
+        {
+            wordCount++;
+        }
+    }
+    if(wordCount < 2)
+    {
+        error.innerHTML = "Invalid name-Must contain at least two words";
         error.style.color = 'red';
-    } 
-    else if (!nameFormat.test(name))
+    }
+    if (!(name[0] >= 'A' && name[0] <= 'Z') && !(name[0] >= 'a' && name[0] <= 'z'))
     {
-        error.innerHTML = "Invalid format. Name must start with a letter, contain at least two words, and may include dot (.) or dash (-)"
+        error.innerHTML = "Invalid name-Must start with a letter";
+        error.style.color = 'red';
+    }
+    else if(!((name >= 'A' && name <= 'Z') || !(name >= 'a' && name <= 'z') || char === '.' || char === '-' || char === ' '))
+    {
+        error.innerHTML = "Invalid name-Only letters, dot (.), and dash (-) are allowed";
         error.style.color = 'red';
     }
     else
     {
-        error.innerHTML = "Valid Name"
+        error.innerHTML = "Valid Name";
         error.style.color = 'green';
     }
 }
