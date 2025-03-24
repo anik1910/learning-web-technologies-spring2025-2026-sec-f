a1bb2fbd449dc8aa1e78b52124f6871004a977dd
Anik <aniktitu@gmail.com>
2025-03-24 14:27:31 +0600

    email.js updated without regexp

--- Lab Task 2/email.js
+++ Lab Task 2/email.js
@@ -1,21 +1,28 @@
-const emailFormat = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 function emailvalidation()
 {
     let email = document.getElementById('email').value;
     let error = document.getElementById('error');
-   if (email == "")
+
+    if (email.trim() == "")
+        {
+            error.innerHTML = "The field is empty";
+            error.style.color = 'red';
+            return;
+        }
+
+    if (!(email[0] >= 'A' && email[0] <= 'Z') && !(email[0] >= 'a' && email[0] <= 'z')&& !(email[0] >= '0' && email[0] <= '9'))
     {
-        error.innerHTML = "The field is empty";
+        error.innerHTML = "Invalid Email-Must start with a letter or number";
         error.style.color = 'red';
-    } 
-    else if (!emailFormat.test(email))
+    }
+    else if(!email.includes('@') || !email.includes('.'))
     {
-        error.innerHTML = "Invalid email format. Use example@example.com"
+        error.innerHTML = "Invalid email-Must contain '@' and '.'";
         error.style.color = 'red';
     }
     else
     {
-        error.innerHTML = "Valid Email"
+        error.innerHTML = "Valid Name";
         error.style.color = 'green';
     }
 }
